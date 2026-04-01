b65202146e7286e6935d99642abb020a301d75e9
Kenan DeHart <kenan.dehart@gmail.com>
2026-04-01 21:54:50 +0000

    Add databse connection pool and /benefits endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import pool from './db.js';
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,11 @@ app.set('json spaces', 2)
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok' });
+});
+
+app.get('/benefits', async (_req, res) => {
+  const result = await pool.query('SELECT * FROM benefits');
+  res.json(result.rows);
 });
 
 app.listen(PORT, () => {
